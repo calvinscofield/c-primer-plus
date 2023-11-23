8735f69ff3e377949e448bebf45496df6df9b775
calvinscofield <calvinscofield@163.com>
2023-11-23 19:32:18 +0800

    16.6

--- 16.6/names_st.h
+++ 16.6/names_st.h
@@ -4,3 +4,16 @@
 #define SLEN 32
 
 // 结构声明
+struct names_st
+{
+    char first[SLEN];
+    char last[SLEN];
+};
+
+// 类型定义
+typedef struct names_st names;
+
+// 函数原型
+oid get_names(names *);
+void show_names(const names *);
+char *s_gets(char *st,int n);
