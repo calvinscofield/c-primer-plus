13bc2ee78e69ab75c5d188d865f205181faedaa9
calvinscofield <calvinscofield@163.com>
2024-02-07 12:16:05 +0800

    ch9

--- 9.8/binary.c
+++ 9.8/binary.c
@@ -1,11 +1,12 @@
+/* binary.c -- 以二进制形式打印制整数 */
 #include <stdio.h>
 void to_binary(unsigned long n);
 
-int main()
+int main(void)
 {
     unsigned long number;
     printf("Enter an integer (q to quit):\n");
-    while(scanf("%lu",&number)==1)
+    while (scanf("%lu", &number) == 1)
     {
         printf("Binary equivalent: ");
         to_binary(number);
@@ -13,15 +14,18 @@ int main()
         printf("Enter an integer (q to quit):\n");
     }
     printf("Done.\n");
+
     return 0;
 }
 
-void to_binary(unsigned long n)
+void to_binary(unsigned long n) /* 递归函数 */
 {
     int r;
-    r=n%2;
-    if(n>=2)
-        to_binary(n/2);
-    putchar(r==0?'0':'1');
-    return;俄
+
+    r = n % 2;
+    if (n >= 2)
+        to_binary(n / 2);
+    putchar(r == 0 ? '0' : '1');
+
+    return;
 }
